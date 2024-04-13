ff32999ad92c27e727303cd131a87a8bf0084971
sustainablelab <sustainablelab@gmail.com>
2024-04-13 13:31:12 -0400

    Line.vector() returns Line as a vector

--- libs/geometry.py
+++ libs/geometry.py
@@ -7,6 +7,12 @@ from dataclasses import dataclass
 
 @dataclass
 class Line:
-    start_pos:tuple
-    end_pos:tuple
+    start:tuple
+    end:tuple
+
+    @property
+    def vector(self) -> tuple:
+        if not self.start: return (None,None)
+        if not self.end: return (None,None)
+        return (self.end[0]-self.start[0], self.end[1]-self.start[1])
 
