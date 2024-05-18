5f60e525f4efbc1b4a235e81225dc5223c028c3e
sustainablelab <sustainablelab@gmail.com>
2024-05-18 13:39:17 -0400

    Draw grid with zoom and scale to fit

--- game_vector_arithmetic.py
+++ game_vector_arithmetic.py
@@ -7,9 +7,20 @@
 [x] Resize window with mouse (when window is not fullscreen) and game art surface resizes
 [x] Game art fills OS window in full screen
 [x] Add DebugHud
+[x] Add dark mode toggle: 'd'
+[x] Add dark and light color schemes
+    * Define 'colors' dict with suffix '_dark' for dark mode and '_light' for light mode
+    * API:
+        * Game has 'property' methods for each color WITHOUT the '_dark'/'_light' suffix
+        * Reading a 'game.color_blah' does the check for dark mode and returns the appropriate color
+[x] Draw grid
+[x] Center grid on screen
+    * 'r' resets the xfm matrix and recenters the grid
+[x] Re-center grid when toggling between fullscreen and windowed
 """
 
 from pathlib import Path
+from dataclasses import dataclass
 import sys
 import atexit
 import logging
@@ -68,7 +79,8 @@ class DebugHud:
         """
         self.debug_text += f"\n{debug_text}"
 
-    def render(self, color=Color(255,255,255)) -> None:
+    def render(self) -> None:
+        color = self.game.color_debug_hud
         mpos = pygame.mouse.get_pos()
         self.text.update(f"FPS: {self.game.clock.get_fps():0.1f} | Window: {self.game.os_window.size} | Mouse: {mpos}"
                          f"{self.debug_text}")
@@ -165,9 +177,10 @@ class OsWindow:
         self._windowed_size = (event.x, event.y)
         self._set_size_and_flags()
 
-def define_settings(game) -> dict:
+def define_settings() -> dict:
     settings = {}
-    settings['setting_debug'] = False
+    settings['setting_debug'] = True
+    settings['setting_dark_mode'] = True
     return settings
 
 def define_surfaces(os_window:OsWindow) -> dict:
@@ -180,7 +193,7 @@ def define_surfaces(os_window:OsWindow) -> dict:
 
     Call this again when toggling fullscreen.
 
-    Do not call this when resizing the window.
+    Do not call this when resizing the window. Instead, Game calls self.update_surfaces().
     """
     surfs = {}                                      # Dict of Pygame Surfaces
 
@@ -197,6 +210,18 @@ def define_surfaces(os_window:OsWindow) -> dict:
 
     return surfs
 
+def define_colors() -> dict:
+    colors = {}
+    colors['color_debug_hud_dark'] = Color(255,255,255)
+    colors['color_debug_hud_light'] = Color(0,0,0)
+    colors['color_graph_paper_bgnd_dark'] = Color(20,20,20)
+    colors['color_graph_paper_bgnd_light'] = Color(180,200,255,255)
+    colors['color_graph_paper_lines_dark'] = Color(100,100,255,50)
+    colors['color_graph_paper_lines_light'] = Color(100,100,255,50)
+    colors['color_green_dark'] = Color(200,255,220)
+    colors['color_brown_light'] = Color(50,30,0)
+    return colors
+
 class Game:
     def __init__(self):
         pygame.init()                                   # Init pygame -- quit in shutdown
@@ -206,9 +231,13 @@ class Game:
         # os.environ["SDL_VIDEO_WINDOW_POS"] = "1000,0"   # Position window in upper right
 
         self.os_window = OsWindow((60*16, 60*9), is_fullscreen=False) # Track OS Window size and flags
-
-        self.settings = define_settings(self)           # Dict of game settings
         self.surfs = define_surfaces(self.os_window)    # Dict of Pygame Surfaces (including pygame.display)
+        pygame.display.set_caption("Vector arithmetic")
+        self.settings = define_settings()               # Dict of game settings
+        self.colors = define_colors()                   # Dict of pygame Colors
+
+        # Game Data
+        self.grid = Grid(self, N=50)
 
         # FPS
         self.clock = pygame.time.Clock()
@@ -218,17 +247,21 @@ class Game:
 
     def game_loop(self) -> None:
         # DebugHud
-        if self.settings['setting_debug']:
-            self.debug_hud = DebugHud(self)
-        else:
-            self.debug_hud = None
+        if self.settings['setting_debug']: self.debug_hud = DebugHud(self)
+        else: self.debug_hud = None
+
+        # Track mouse position in game coordinates
+        if self.debug_hud:
+            mpos_p = pygame.mouse.get_pos()             # Mouse in pixel coord sys
+            mpos_g = self.grid.xfm_pg(mpos_p)           # Mouse in game coord sys
+            self.debug_hud.add_text(f"Mouse (game): {mpos_g}")
 
         # UI
         self.handle_ui_events()
 
         # Game art
-        bgnd_color = pygame.color.Color(100,100,100)
-        self.surfs['surf_game_art'].fill(bgnd_color)
+        self.surfs['surf_game_art'].fill(self.color_graph_paper_bgnd)
+        self.grid.draw(self.surfs['surf_game_art'])
 
         # Copy game art to OS window
         ### pygame.Surface.blit(source, dest, area=None, special_flags=0) -> Rect
@@ -271,7 +304,15 @@ class Game:
                     self.os_window.handle_WINDOWRESIZED(event) # Update OS window size
                     self.update_surfaces() # Update surfaces affected by OS window size
                     logger.debug(f"game art: {self.surfs['surf_game_art'].get_size()}")
+                    # Resize and recenter the grid
+                    self.grid.reset()
                 case pygame.KEYDOWN: self.handle_keydown(event)
+                case pygame.MOUSEWHEEL:
+                    ### {'flipped': False, 'x': 0, 'y': 1, 'precise_x': 0.0, 'precise_y': 1.0, 'touch': False, 'window': None}
+                    match event.y:
+                        case 1: self.grid.zoom_in()
+                        case -1: self.grid.zoom_out()
+                        case _: pass
                 # Log any other events
                 case _:
                     logger.debug(f"Ignored event: {pygame.event.event_name(event.type)}")
@@ -284,17 +325,193 @@ class Game:
                 self.os_window.toggle_fullscreen() # F11 - toggle fullscreen
                 self.surfs = define_surfaces(self.os_window)
                 logger.debug(f"game art: {self.surfs['surf_game_art'].get_size()}")
+                # Resize and recenter the grid
+                self.grid.reset()
             case pygame.K_F2:
                 self.settings['setting_debug'] = not self.settings['setting_debug']
                 logger.debug(f"Debug: {self.settings['setting_debug']}")
+            case pygame.K_d: self.toggle_dark_mode()
+            case pygame.K_r: self.grid.reset()
 
             case _:
                 logger.debug(f"{event.unicode}")
 
     def update_surfaces(self) -> None:
-        """See 'define_surfaces()'"""
+        """Call this after os_window handles WINDOWRESIZED event. See 'define_surfaces()'"""
         self.surfs['surf_game_art'] = pygame.Surface(self.os_window.size, flags=pygame.SRCALPHA)
         self.surfs['surf_draw'] = pygame.Surface(self.os_window.size, flags=pygame.SRCALPHA)
+
+    def toggle_dark_mode(self) -> None:
+        self.settings['setting_dark_mode'] = not self.settings['setting_dark_mode']
+
+    @property
+    def color_debug_hud(self) -> Color:
+        if self.settings['setting_dark_mode']:
+            return self.colors['color_debug_hud_dark']
+        else:
+            return self.colors['color_debug_hud_light']
+
+    @property
+    def color_graph_paper_bgnd(self) -> Color:
+        if self.settings['setting_dark_mode']:
+            return self.colors['color_graph_paper_bgnd_dark']
+        else:
+            return self.colors['color_graph_paper_bgnd_light']
+
+    @property
+    def color_graph_paper_lines(self) -> Color:
+        if self.settings['setting_dark_mode']:
+            return self.colors['color_graph_paper_lines_dark']
+        else:
+            return self.colors['color_graph_paper_lines_light']
+
+@dataclass
+class LineSeg:
+    start:tuple
+    end:tuple
+
+    @property
+    def vector(self) -> tuple:
+        return (self.end[0] - self.start[0], self.end[1] - self.start[1])
+
+class Grid:
+    """Define a grid of lines.
+
+    :param N:int -- number of grid lines (grid is NxN)
+    """
+    def __init__(self, game:Game, N:int):
+        self.game = game
+        self.N = N
+        self.scale = 1.0 # zoom
+        self.reset()
+
+    def reset(self) -> None:
+        """Reset to initial transformation matrix: top-down view, grid centered.
+
+        Define a 2x3 transformation matrix [a,b,e;c,d,f] to
+        go from g (game grid) to p (pixels).
+        """
+        # Define 2x2 transform
+        # Use a top-down view
+        self.a = 20
+        self.b = 0
+        self.c = 0
+        self.d = -20
+
+        # Define offset vector (in pixel coordinates)
+        # Place origin at center of game art
+        ctr = (int(self.game.os_window.size[0]/2),
+               int(self.game.os_window.size[1]/2))
+        self.e = ctr[0]
+        self.f = ctr[1]
+
+        self.scale = self.zoom_to_fit()
+
+    def zoom_to_fit(self) -> float:
+        # Get the size of the grid
+        size_g = (self.N, self.N)
+        
+        # Get an unscaled 2x2 transformation matrix
+        a,b,c,d = self.a, self.b, self.c, self.d
+
+        # Transform the size to pixel coordinates (as if the size were a point)
+        size_p = (a*size_g[0] + b*size_g[1], c*size_g[0] + d*size_g[1])
+
+        # Add some margin
+        size_p = (abs(size_p[0]) + 20, abs(size_p[1]) + 20)
+
+        scale_x = self.game.os_window.size[0]/size_p[0]
+        scale_y = self.game.os_window.size[1]/size_p[1]
+        logger.debug(f"scale: {scale_x}, {scale_y}")
+
+        return min(scale_x, scale_y)
+
+    def scaled(self) -> tuple:
+        return (self.a*self.scale, self.b*self.scale, self.c*self.scale, self.d*self.scale)
+
+    @property
+    def det(self) -> float:
+        a,b,c,d = self.scaled()
+        det = a*d-b*c
+        if det == 0:
+            # If det=0, Ainv will have div by 0, so just make det very small.
+            return 0.0001
+        else:
+            return a*d-b*c
+
+    def xfm_gp(self, point:tuple) -> tuple:
+        """Transform point from game grid coordinates to OS Window pixel coordinates."""
+        # Define 2x2 transform
+        a,b,c,d = self.scaled()
+        # Define offset vector (in pixel coordinates)
+        e,f = (self.e, self.f)
+        return (a*point[0] + b*point[1] + e, c*point[0] + d*point[1] + f)
+
+    def xfm_pg(self, point:tuple, p:int=0) -> tuple:
+        """Transform point from OS Window pixel coordinates to game grid coordinates.
+
+        :param point:tuple -- (x,y) in pixel coordinates
+        :param p:int -- decimal precision of returned coordinate (default: 0, return ints)
+        :return tuple -- (x,y) in grid goordinates
+        """
+        # Define 2x2 transform
+        a,b,c,d = self.scaled()
+        # Define offset vector (in pixel coordinates)
+        e,f = (self.e, self.f)
+        # Calculate the determinant of the 2x2
+        det = self.det
+        g = ((   d/det)*point[0] + (-1*b/det)*point[1] + (b*f-d*e)/det,
+             (-1*c/det)*point[0] + (   a/det)*point[1] + (c*e-a*f)/det)
+        # Define precision
+        if p==0:
+            return (int(round(g[0])), int(round(g[1])))
+        else:
+            return (round(g[0],p), round(g[1],p))
+
+    def zoom_in(self) -> None:
+        self.scale *= 1.1
+
+    def zoom_out(self) -> None:
+        self.scale *= 0.9
+
+    @property
+    def hlinesegs(self) -> list:
+        """Return list of horizontal line segments."""
+        ### Put origin in center
+        a = -1*int(self.N/2)
+        b = int(self.N/2)
+        cs = list(range(a,b+1))
+        hls = []
+        for c in cs:
+            hls.append(LineSeg(start=(a,c), end=(b,c)))
+        return hls
+
+    @property
+    def vlinesegs(self) -> list:
+        ### Put origin in center
+        a = -1*int(self.N/2)
+        b = int(self.N/2)
+        cs = list(range(a,b+1))
+        vls = []
+        for c in cs:
+            vls.append(LineSeg(start=(c,a), end=(c,b)))
+        return vls
+
+    def draw(self, surf:pygame.Surface) -> None:
+        color = self.game.color_graph_paper_lines
+        linesegs = self.hlinesegs + self.vlinesegs
+        for grid_line in linesegs:
+            ### Anti-aliased:
+            ### aaline(surface, color, start_pos, end_pos, blend=1) -> Rect
+            ### Blend is 0 or 1. Both are anti-aliased.
+            ### 1: (this is what you want) blend with the surface's existing pixel color
+            ### 0: completely overwrite the pixel (as if blending with black)
+            pygame.draw.aaline(surf, color, 
+                    self.xfm_gp(grid_line.start),
+                    self.xfm_gp(grid_line.end),
+                    blend=1                             # 0 or 1
+                    )
+
 
 if __name__ == '__main__':
     print(f"Run {Path(__file__).name}")
